38cf0167d4fd23a6d94769659fa86600c7e6eaf1
GregPappDev <gregpappdev@gmail.com>
2024-01-12 23:14:21 +0100

    add WebElements of 'Log In' page

--- src/main/java/pages/LogInPage.java
+++ src/main/java/pages/LogInPage.java
@@ -1,4 +1,27 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LogInPage {
+
+    public LogInPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+    }
+
+    private String url = "https://www.saucedemo.com/";
+
+    @FindBy(id = "user-name")
+    private WebElement userNameInputField;
+
+    @FindBy(id = "password")
+    private WebElement passwordInputField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//*[contains(text(), 'Epic sadface: Password is required')]")
+    private WebElement loginErrorMessage;
 }
