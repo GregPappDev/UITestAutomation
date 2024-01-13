335632104f9ccfe044b7996e15511b75deac52fb
GregPappDev <gregpappdev@gmail.com>
2024-01-13 21:59:46 +0100

    add positive LoginTest to RemoteWebDriver test

--- src/main/java/services/LogInService.java
+++ src/main/java/services/LogInService.java
@@ -4,76 +4,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LogInPage;
-import org.apache.logging.log4j.Logger;
+
 
 public class LogInService {
 
     final private WebDriver driver;
     final private LogInPage loginPage;
     final private WebDriverWait wait;
-    final private Logger logger;
 
-    public LogInService(WebDriver driver, LogInPage loginPage, WebDriverWait wait, Logger logger) {
+
+    public LogInService(WebDriver driver, LogInPage loginPage, WebDriverWait wait) {
         this.driver = driver;
         this.loginPage = loginPage;
         this.wait = wait;
-        this.logger = logger;
     }
 
-    public void loginToWebsite(String userName, String password){
+    public void loginToWebsite(String userName, String password) throws InterruptedException {
         navigateToUrl();
         enterUserName(userName);
         enterPassword(password);
         clickLoginButton();
+
     }
 
     private void navigateToUrl() {
-        try
-        {
-            driver.get(loginPage.getUrl());
-        }
-        catch (Exception e){
-           logger.info("An error occurred while accessing the URL");
-           logger.error("Error message", e);
-        }
+        driver.get(loginPage.getUrl());
 
     }
     private void enterUserName(String userName) {
-        try
-        {
-            wait.until(ExpectedConditions.elementToBeSelected(loginPage.getUserNameInputField()));
-            loginPage.getUserNameInputField().sendKeys(userName);
-        }
-        catch (Exception e)
-        {
-            logger.info("An error occurred while trying to locate 'username' field");
-            logger.error("Error message", e);
-        }
+        loginPage.getUserNameInputField().sendKeys(userName);
+
     }
 
     private void enterPassword(String password) {
-        try
-        {
-            wait.until(ExpectedConditions.elementToBeSelected(loginPage.getPasswordInputField()));
-            loginPage.getPasswordInputField().sendKeys(password);
-        }
-        catch (Exception e)
-        {
-            logger.info("An error occurred while trying to locate 'password' field");
-            logger.error("Error message", e);
-        }
+      loginPage.getPasswordInputField().sendKeys(password);
+
     }
 
     private void clickLoginButton() {
-        try
-        {
-            wait.until(ExpectedConditions.elementToBeClickable(loginPage.getLoginButton()));
-            loginPage.getLoginButton().click();
-        }
-        catch (Exception e)
-        {
-            logger.info("An error occurred while trying to locate 'login' button");
-            logger.error("Error message", e);
-        }
+        loginPage.getLoginButton().click();
+
     }
+
 }
--- src/test/java/testcases/LogInTest.java
+++ src/test/java/testcases/LogInTest.java
@@ -1,4 +1,69 @@
 package testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.LogInPage;
+import services.LogInService;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LogInTest {
+
+    public WebDriver driver;
+    private LogInService logInService;
+    private LogInPage logInPage;
+    private WebDriverWait wait;
+
+    @Parameters("browser")
+    @BeforeTest
+
+    public void setup(String browserType) throws MalformedURLException{
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if(browserType.equalsIgnoreCase("chrome")){
+            capabilities.setBrowserName(BrowserType.CHROME);
+            System.out.println(("Test Case Execution Started On ==> " + browserType));
+        } else if(browserType.equalsIgnoreCase("firefox")){
+            capabilities.setBrowserName(BrowserType.FIREFOX);
+            System.out.println(("Test Case Execution Started On ==> " + browserType));
+        } else {
+            capabilities.setBrowserName(BrowserType.EDGE);
+            System.out.println(("Test Case Execution Started On ==> " + browserType));
+        }
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4445"), capabilities);
+        logInPage = new LogInPage(driver);
+        wait = new WebDriverWait(driver, 5);
+        logInService = new LogInService(driver, logInPage, wait);
+    }
+
+
+    @Test
+    public void LoginToWebsite_Successful_WithValidCredentials() throws InterruptedException {
+
+        logInService.loginToWebsite("standard_user", "secret_sauce");
+
+        String title = driver.findElement(By.className("title")).getText();
+
+        Assert.assertEquals(title, "Products");
+
+    }
+
+
+    @AfterTest
+    public void closeDriver(){
+        driver.quit();
+    }
 }
