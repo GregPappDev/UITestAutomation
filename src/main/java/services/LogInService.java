5bdfa6a158f8dcf0183386b12f7f869f138926d7
GregPappDev <gregpappdev@gmail.com>
2024-01-13 16:23:43 +0100

    add logger and exception handling to LogInService

--- src/main/java/services/LogInService.java
+++ src/main/java/services/LogInService.java
@@ -1,19 +1,23 @@
 package services;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.LogInPage;
+import org.apache.logging.log4j.Logger;
 
 public class LogInService {
 
     final private WebDriver driver;
     final private LogInPage loginPage;
     final private WebDriverWait wait;
+    final private Logger logger;
 
-    public LogInService(WebDriver driver, WebDriverWait wait) {
+    public LogInService(WebDriver driver, LogInPage loginPage, WebDriverWait wait, Logger logger) {
         this.driver = driver;
-        loginPage = new LogInPage(driver);
+        this.loginPage = loginPage;
         this.wait = wait;
+        this.logger = logger;
     }
 
     public void loginToWebsite(String userName, String password){
@@ -24,17 +28,52 @@ public class LogInService {
     }
 
     private void navigateToUrl() {
-        driver.get(loginPage.getUrl());
+        try
+        {
+            driver.get(loginPage.getUrl());
+        }
+        catch (Exception e){
+           logger.info("An error occurred while accessing the URL");
+           logger.error("Error message", e);
+        }
+
     }
     private void enterUserName(String userName) {
-        loginPage.getUserNameInputField().sendKeys(userName);
+        try
+        {
+            wait.until(ExpectedConditions.elementToBeSelected(loginPage.getUserNameInputField()));
+            loginPage.getUserNameInputField().sendKeys(userName);
+        }
+        catch (Exception e)
+        {
+            logger.info("An error occurred while trying to locate 'username' field");
+            logger.error("Error message", e);
+        }
     }
 
     private void enterPassword(String password) {
-        loginPage.getPasswordInputField().sendKeys(password);
+        try
+        {
+            wait.until(ExpectedConditions.elementToBeSelected(loginPage.getPasswordInputField()));
+            loginPage.getPasswordInputField().sendKeys(password);
+        }
+        catch (Exception e)
+        {
+            logger.info("An error occurred while trying to locate 'password' field");
+            logger.error("Error message", e);
+        }
     }
 
     private void clickLoginButton() {
-        loginPage.getLoginButton().click();
+        try
+        {
+            wait.until(ExpectedConditions.elementToBeClickable(loginPage.getLoginButton()));
+            loginPage.getLoginButton().click();
+        }
+        catch (Exception e)
+        {
+            logger.info("An error occurred while trying to locate 'login' button");
+            logger.error("Error message", e);
+        }
     }
 }
