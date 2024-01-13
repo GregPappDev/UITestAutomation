e6846583ae1d37ffeb11c6ef575808c73bc2b20a
GregPappDev <gregpappdev@gmail.com>
2024-01-13 20:16:28 +0100

    add Chrome, Firefox and Edge options to RemoteWebDriverSetup

--- src/test/java/utils/RemoteWebDriverSetup.java
+++ src/test/java/utils/RemoteWebDriverSetup.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,7 +16,15 @@ public class RemoteWebDriverSetup {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
+        if(browserType.equalsIgnoreCase("chrome")){
+            capabilities.setBrowserName(BrowserType.CHROME);
+        } else if(browserType.equalsIgnoreCase("firefox")){
+            capabilities.setBrowserName(BrowserType.FIREFOX);
+        } else {
+            capabilities.setBrowserName(BrowserType.EDGE);
+        }
 
+        System.out.println(("Test Case Execution Started On ==> " + browserType));
         driver = new RemoteWebDriver(new URL("http://localhost:4445"), capabilities);
 
         return driver;
